b1897f5aa8658ea9260ef3ee72492d6a1b38ec9e
irof <irof@hogedriven.net>
2013-06-02 07:48:27 +0900

    tasknameでwebdriverを切り替える

--- calc-geb/src/test/groovy/GebConfig.groovy
+++ calc-geb/src/test/groovy/GebConfig.groovy
@@ -2,16 +2,12 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.chrome.ChromeDriver
 
-driver = { new FirefoxDriver() }
-
 environments {
 
-    // grails -Dgeb.env=chrome test-app
     chrome {
         driver = { new ChromeDriver() }
     }
 
-    // grails -Dgeb.env=firefox test-app
     firefox {
         driver = { new FirefoxDriver() }
     }
